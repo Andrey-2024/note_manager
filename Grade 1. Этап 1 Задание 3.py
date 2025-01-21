34c63b8f21660d1edbd762c763fbd16651a734d0
Andrey-2024 <156535693+Andrey-2024@users.noreply.github.com>
2025-01-21 22:57:13 +0500

    Add files via upload

---
+++ Grade 1. Этап 1 Задание 3.py
@@ -0,0 +1,9 @@
+# title Grade 1. Этап 1: Задание 3
+
+title = input("Введите заголовок заметки: ")
+
+content = input("Введите содержимое заметки: ")
+
+date = input("Введите дату заметки (в формате YYYY-MM-DD): ")
+
+print(title, content, date)
